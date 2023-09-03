234209c08eac85884c6d830d9629a3ec4f9d742a
franki <8024ldbryant@gmail.com>
2023-09-03 17:30:12 +0800

    feat: use define text

--- c-programming-language-examples-and-exercise/1.2-变量与算术表达式.c
+++ c-programming-language-examples-and-exercise/1.2-变量与算术表达式.c
@@ -27,6 +27,10 @@
 //   return 0;
 // }
 
+#define LOWER 0   // lower limit of temperature scale
+#define UPPER 300 // upper limit
+#define STEP 20  // step size
+
 /**
  * @brief 浮点数版本
  *
@@ -37,16 +41,16 @@ int main()
   float fachr, celsius;
   float lower, upper, step;
 
-  lower = 0;   // lower limit of temperature scale
-  upper = 300; // upper limit
-  step = 20;   // step size
+  // lower = 0;   // lower limit of temperature scale
+  // upper = 300; // upper limit
+  // step = 20;   // step size
 
-  fachr = lower;
-  while (fachr <= upper)
+  fachr = LOWER;
+  while (fachr <= UPPER)
   {
     celsius = (5.0 / 9.0) * (fachr - 32.0);
     printf("%3.0f %6.1f\n", fachr, celsius);
-    fachr = fachr + step;
+    fachr = fachr + STEP;
   }
 
   return 0;
