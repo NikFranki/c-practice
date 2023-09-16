a20aa773d470130bfd6fbf8aba38bb39350ed1bf
franki <8024ldbryant@gmail.com>
2023-09-16 17:02:20 +0800

    feat: add c100-6

---
+++ c100/6.c
@@ -0,0 +1,22 @@
+/**
+ * @file 6.c
+ * @author your name (you@domain.com)
+ * @brief 用*号输出字母C的图案。
+ * @version 0.1
+ * @date 2023-09-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf(" ****\n");
+  printf(" *\n");
+  printf(" *\n");
+  printf(" ****\n");
+
+  return 0;
+}
